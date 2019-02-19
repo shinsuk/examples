1486eac451c331eb8908cbe62b040a93a192fecc
Bumseok <shinsuk@gwbs.net>
2019-02-19 15:40:03 +0900

    Create a project for Menubar App

---
+++ macOS/Quotes/Quotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Bumseok Lee on 20190219.
+//  Copyright © 2019  口月本心 Computer Club. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS/Quotes/Quotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Bumseok Lee on 20190219.
+//  Copyright © 2019  口月本心 Computer Club. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
