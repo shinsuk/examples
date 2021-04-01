a96a9504b545e719434338448625f92fa1272164
Bumseok <bumseok.lee@gmail.com>
2021-04-02 01:14:56 +0900

    Create a project for SwiftUI App

---
+++ MenubarApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenubarApp
+//
+//  Created by Linus on 20210402.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenubarApp/MenubarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenubarAppApp.swift
+//  MenubarApp
+//
+//  Created by Linus on 20210402.
+//
+
+import SwiftUI
+
+@main
+struct MenubarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
